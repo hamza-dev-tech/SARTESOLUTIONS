f5c243fdc9e588f7fdb66823c3ec56c3f6842d23
hamza-dev-tech <hamzadevtech01@gmail.com>
2024-07-26 01:36:34 +0500

    new

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
     images: {
       domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
     }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-import GithubProvider from "next-auth/providers/github";
+
 import GoogleProvider from "next-auth/providers/google";
 import { getServerSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -6,16 +6,24 @@ import prisma from "./connect";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  database: process.env.DATABASE_URL,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    })
   ],
+  session: {
+    jwt: true,
+  },
+  debug: true,
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
