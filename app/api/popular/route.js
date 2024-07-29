bc5df2fc72605205109fc69634ef0b543654fd37
hamza-dev-tech <hamzadevtech01@gmail.com>
2024-07-30 01:39:05 +0500

    s

--- app/api/popular/route.js
+++ app/api/popular/route.js
@@ -2,23 +2,31 @@ import prisma from "@/src/utils/connect";
 import { NextResponse } from "next/server";
 
 
-// Get Popular Posts
+
+
+
+
 export const GET = async (req) => {
+
+
+  const query = {
+    orderBy: {
+      views: 'desc',
+    },
+    take: 4,
+  };
+
   try {
-    const posts = await prisma.post.findMany({
-      orderBy: {
-        views: 'desc',
-      },
-      take: 4,
-    });
-    return new NextResponse(
-      JSON.stringify({ posts }),
-      { status: 200, headers: { 'Cache-Control': 'no-store' } }
-    );
+    const [ posts ] = await prisma.$transaction([
+      prisma.post.findMany(query),
+
+    ]);
+    return new NextResponse(JSON.stringify({ posts }), { status: 200 });
   } catch (err) {
     console.log(err);
     return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }), { status: 500 }
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
     );
   }
 };
