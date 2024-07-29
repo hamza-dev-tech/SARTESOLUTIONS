c6a2a0515ad5fa9fc2c84ac599b8a132ca4012cb
hamza-dev-tech <hamzadevtech01@gmail.com>
2024-07-29 22:13:02 +0500

    k

--- app/api/picked/route.js
+++ app/api/picked/route.js
@@ -1,7 +1,7 @@
 import prisma from "@/src/utils/connect";
 import { NextResponse } from "next/server";
 
-// Get the featured post
+// Get the Picked posts
 export const GET = async (req) => {
   try {
     const posts = await prisma.post.findMany({
@@ -9,7 +9,10 @@ export const GET = async (req) => {
       take: 4,
     });
 
-    return new NextResponse(JSON.stringify({ posts }), { status: 200 });
+    return new NextResponse(
+      JSON.stringify({ posts }),
+      { status: 200, headers: { 'Cache-Control': 'no-store' } }
+    );
   } catch (err) {
     console.log(err);
     return new NextResponse(
--- app/api/popular/route.js
+++ app/api/popular/route.js
@@ -1,6 +1,8 @@
 import prisma from "@/src/utils/connect";
 import { NextResponse } from "next/server";
 
+
+// Get Popular Posts
 export const GET = async (req) => {
   try {
     const posts = await prisma.post.findMany({
@@ -9,7 +11,10 @@ export const GET = async (req) => {
       },
       take: 4,
     });
-    return new NextResponse(JSON.stringify({ posts }), { status: 200 });
+    return new NextResponse(
+      JSON.stringify({ posts }),
+      { status: 200, headers: { 'Cache-Control': 'no-store' } }
+    );
   } catch (err) {
     console.log(err);
     return new NextResponse(
